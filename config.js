34347ac9c197eedd9513959a4509e086352cbad5
wkswoks <80533791+wkswoks@users.noreply.github.com>
2021-04-07 14:05:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "token": "Botunuzun tokeni buraya gelecek!",
-  "geliştiriciler": ["Kendi Discord ID'nizi buraya yazın."], 
-  "prefix": "Botunuzun prefixi.",
+  "token": "ODI5MzA1NzI4NjkwNTUyODgy.YG2NJQ.GrxP0JB5idMudLXMvmtHSITI4Lc",
+  "geliştiriciler": ["763666241327988747"], 
+  "prefix": ".",
 
- "sunucuid": "",
- "kayitKanal": "",
- "genelChat": "",
+ "sunucuid": "825820535292297216",
+ "kayitKanal": "827482597647056916",
+ "genelChat": "826724939103993857",
 
- "kizRol": "",
- "vip": "",
- "erkekRol": "",
- "kayitsiz": "",
- "teyitci": ""
+ "kizRol": "829298150984253460",
+ "vip": "826762428930719764",
+ "erkekRol": "829298234298990603",
+ "kayitsiz": "826764617136603136",
+ "teyitci": "829310358568697897"
 };
